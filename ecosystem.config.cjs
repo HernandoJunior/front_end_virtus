a3f99782c9efb88d9764d1527fc8a918e428a3b1
HernandoJunior <151379697+HernandoJunior@users.noreply.github.com>
2025-10-18 12:07:51 -0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
       PM2_SERVE_PATH: './dist',
       PM2_SERVE_PORT: process.env.PORT || 8080,
       PM2_SERVE_SPA: 'true',
-      PM2_SERVE_HOMEPAGE: '/index.html'
+      PM2_SERVE_HOMEPAGE: '/index.html',
+      VITE_API_URL: process.env.VITE_API_URL || 'https://my-spring-api.azurewebsites.net'
     }
   }]
 };
