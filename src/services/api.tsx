97e86ee5d624eee58182e52c363179ded649b4fa
= <devhernandojunior@gmail.com>
2026-01-23 16:07:22 -0300

    rebuild project

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,9 +1,8 @@
-// src/services/api.ts
 import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
-  withCredentials: true, // CRUCIAL: Envia cookies automaticamente
+  withCredentials: true, 
 });
 
 let isRefreshing = false;
